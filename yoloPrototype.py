589bd5c912a87f32d214f142d10a7d7f3e3e9b1c
Matthew <mattbhubbs@knights.ucf.edu>
2022-02-23 16:46:09 -0500

    first shot at using torchhub

--- yoloPrototype.py
+++ yoloPrototype.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append("./yolov5/models/")
 from common import DetectMultiBackend
 import torch
+import cv2 as cv
 import os
 from  PIL import Image
 import numpy as np
@@ -11,33 +12,27 @@ def main():
     # print to make sure we're entering main and in the directory we expect, and have cuda availible
     print("current working irectory: " + os.getcwd() + "\n")
     print("cuda is availible?: " + str(torch.cuda.is_available()))
-    # make instance of yolo model, give it weights to load, also set device
+    # Model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = DetectMultiBackend(weights="bestPizza500.pt")
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
     model.to(device)
 
     # load an image. in the real thing, this would be a parameter
     # im_frame = Image.open("bb8_sample28.png") # true positive, bb8 is in this picture
     im_frame = Image.open("bb8_paint_true_neg.png") # true negative, bb8 is not in this image
-    np_frame = np.array(im_frame)
 
-    # change to be tensor with axes to be (batch, channels, height, width)
-    np_frame = np.transpose(np_frame, axes=[2,0,1])
-    inputTensor = torch.tensor(np_frame).to(device)
-    inputTensor = torch.unsqueeze(inputTensor, 0)
-    inputTensor = inputTensor.type(torch.cuda.FloatTensor)
+    # all we have to do is wrap the image into a batch. I guess torch hub deals with reordering data for us?
+    imgs = [im_frame]
+    
+    # Inference
+    results = model(imgs, size=640)  # includes NMS
 
-    # if this isn't ([1, 3, 512, 512]) then youre gonna have an error
-    print("image shape: " + str(inputTensor.shape))
+    # show shapes ad results
+    print(imgs.shape)
+    print(results.shape)
+    print(results)
 
-    # and this is why i hate weakly typed languages. What is result? WHO KNOWS???
-    result = model.forward(inputTensor)
-
-    print(result)
-    print(result.shape)
-
-    confidence = torch.squeeze(result)
-    confidence = confidence[:,5] # the sixth column should be the confidence scores 
+    confidence = results[:, :, 5] # the sixth column should be the confidence scores 
     max_confidence = torch.max(confidence)
 
     print(confidence.shape)
